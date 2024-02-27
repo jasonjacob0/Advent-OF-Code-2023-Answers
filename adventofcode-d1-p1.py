54e6a0762cda822aa1c22e4df711ce82034e7c89
jasonjacob0 <100529718+jasonjacob0@users.noreply.github.com>
2024-02-27 22:14:56 +0530

    Update adventofcode-d1-p1.py

--- adventofcode-d1-p1.py
+++ adventofcode-d1-p1.py
@@ -2,10 +2,8 @@ a =''' 1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet'''
-
-p = a.split("\n")
 result=[]
-for i in p:
+for i in a.split("\n"):
     k = [int(j) for j in i if j.isdigit()]
     if len(k)>=1:
         result.append(k[0]*10+k[-1])
